78f28b56f7db67596c93aca28a99faa95f0b4dde
JAJR Construction <tolentinochristian89@gmail.com>
2026-02-10 11:18:51 +0800

    gfdgdf

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://procurement.xandree.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}));
+app.options('*', cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
